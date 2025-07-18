58e374162f13dc9f61751bd54a47e408343718db
Gzuz <MRRbrown@protonmail.com>
2025-07-19 00:54:58 +0200

    Adding the simple header

---
+++ header.php
@@ -0,0 +1,3 @@
+<header class="bg-gradient-to-r from-blue-600 to-blue-800 px-6 py-6 shadow-md">
+    <h1 class="text-2xl font-bold text-white text-center"><?php echo htmlspecialchars($page_title) ?? 'Tableau de Bord ODD' ?></h1>
+</header>
